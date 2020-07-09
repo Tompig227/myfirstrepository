5161573c14a18af810573e321914e6fcd0df20b4
Tompig <streamer.thuner@foxmail.com>
2020-07-09 21:47:29 +0800

    ID3算法中D3算法中计算信息熵

--- test/design.py
+++ test/design.py
@@ -16,12 +16,18 @@ dataSet = [['青年', '否', '否', '一般', '拒绝'],
                 ['老年', '否', '否', '一般', '拒绝'],
                 ]
 featureName = ['年龄', '有工作', '有房子', '信贷情况']
+numlength=len(dataSet)
 labelCounts={}
 for featVec in dataSet:
         curruntLabel = featVec[-1]
         if curruntLabel not in labelCounts.keys():
                 labelCounts[curruntLabel]=0
         labelCounts[curruntLabel]+=1
+shannonEnt=0.0
+for key in labelCounts:
+        prob=float(labelCounts[key])/numlength
+        shannonEnt-=prob*log(prob,2)
+print(shannonEnt)
 print (labelCounts)
 classList = [example[-1] for example in dataSet] # 类别列表
 print(classList)
